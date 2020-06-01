f73ccd63f1cf1c37c64118b1ed337441796e90fa
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-06-01 19:35:43 +0300

    Model ve Repository gelistirildi

---
+++ NLayerProject.Core/Model/Category.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NLayerProject.Core.Model
+{
+   public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NLayerProject.Core/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entities);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+    }
+}
