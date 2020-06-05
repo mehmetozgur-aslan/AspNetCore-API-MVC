d7c69b9fb35548177eeee514718597a61e966a63
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-06-05 08:06:39 +0300

    Veirtabaninin olusturulmasi

---
+++ NLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using NLayerProject.Core.Repositories;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Data.UnitOfWorks
+{
+   public class UnitOfWork:IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        private ProductRepository _productRepository;
+        private CategoryRepository _CategoryRepository;
+
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _CategoryRepository = _CategoryRepository ?? new CategoryRepository(_context);
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
