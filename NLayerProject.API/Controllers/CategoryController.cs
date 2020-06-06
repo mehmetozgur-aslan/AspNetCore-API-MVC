e86a9127ef3e2f87c4999bd39cbd22c3370b517c
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-06-06 19:16:22 +0300

    Category Controller

---
+++ NLayerProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.API.DTOs;
+using NLayerProject.Core.Models;
+using NLayerProject.Core.Services;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoryDto = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+
+            return Ok(categoryDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+
+            return Ok(categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+            var newCategory = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+
+            return Created(String.Empty, _mapper.Map<CategoryDto>(newCategory));
+        }
+
+        [HttpPut]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(_mapper.Map<Category>(categoryDto));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}/products")]
+        public async Task<IActionResult> GetWithProductsById(int id)
+        {
+            var category = await _categoryService.GetWithProductsByIdAsync(id);
+
+            return Ok(_mapper.Map<CategoryWithProductDto>(category));
+        }
+
+    }
+}
--- NLayerProject.API/Startup.cs
+++ NLayerProject.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,6 +34,8 @@ namespace NLayerProject.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IService<>), typeof(Service<>));
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
