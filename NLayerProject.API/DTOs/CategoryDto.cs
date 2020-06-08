50aa7889bc2ad26118d629bcb1c841639badf9a4
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-06-08 16:54:12 +0300

    CategoryController CRUD islemleri

--- NLayerProject.API/DTOs/CategoryDto.cs
+++ NLayerProject.API/DTOs/CategoryDto.cs
@@ -9,7 +9,7 @@ namespace NLayerProject.API.DTOs
     public class CategoryDto
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="{0} alanı boş olamaz.")]
         public string Name { get; set; }
     }
 }
--- NLayerProject.WebUI/Controllers/CategoryController.cs
+++ NLayerProject.WebUI/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Models;
 using NLayerProject.Core.Services;
 using NLayerProject.WebUI.DTOs;
 
@@ -25,6 +26,43 @@ namespace NLayerProject.WebUI.Controllers
             var categories = await _categoryService.GetAllAsync();
 
             return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+
+            return RedirectToAction("Index");
+        }
+
+        //update/5
+        public async Task<IActionResult> Update(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            return View(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpPost]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            _categoryService.Update(_mapper.Map<Category>(categoryDto));
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+
+            return RedirectToAction("Index");
         }
     }
 }
